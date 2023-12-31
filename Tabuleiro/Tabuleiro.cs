ac8862beca5a2f106224b80d7dd4d084f9574528
Luan Gobo <luangobo@hotmail.com>
2023-12-31 14:37:59 -0300

    Comecando a implementar o tabuleiro

---
+++ Tabuleiro/Tabuleiro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xadrez_console.Tabuleiro
+{
+    internal class Tabuleiro
+    {
+        public int linhas { get; set; }
+        public int colunas { get; set; }
+        private Peca[,] pecas;
+
+        public Tabuleiro(int linha, int colunas)
+        {
+            this.linhas = linha;
+            this.colunas = colunas;
+            pecas = new Peca[linha,colunas];
+        }
+    }
+}
