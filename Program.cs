b373b09f834e697496fa6328f1d7cd1fb1636493
Luan Gobo <luangobo@hotmail.com>
2024-01-02 20:33:51 -0300

    Implementando as pecas Rei e Torre

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using tabuleiro;
+using xadrez;
 
 namespace xadrez_console
 {
@@ -8,6 +9,11 @@ namespace xadrez_console
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
+
+            tab.colocarpeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            tab.colocarpeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            tab.colocarpeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+
 
             Tela.imprimirTabuleiro(tab);
 
